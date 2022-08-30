70819dfb5b0bddcdd55d5db264d8e70af10e9e37
0xh377w4v3 <0x.antonio.mesquita@gmail.com>
2022-08-30 17:20:51 -0300

    Correção no script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,31 +1,26 @@
 window.onload = function(){
-  //pegar o botão adicionar
-  const BOTAO_ENVIAR = document.querySelector('#enviar')
+  //pegar os elementos e adicionar em uma variavel
+  const FORMULARIO   = document.querySelector('#enviar')
+  const NOME_DA_BANDA = document.getElementById('exampleInputText1')
+  const DESCRICAO_DA_BANDA = document.getElementById('exampleInputText2')
+  const IMAGEM_DA_BANDA = document.getElementById('exampleInputText3')
   
-  //adicionar evento ao clicar no botão 'adicionar'
-  BOTAO_ENVIAR.addEventListener('click', event => {
-    event.preventDefault()
-    
-    //pegar dados do formulario
-    let nomeDaBanda = document.getElementById('exampleInputText1').value
-    let descricaoDaBanda = document.getElementById('exampleInputText2').value
-    let imgDaBanda = document.getElementById('exampleInputText3').value
-
-    //console.log(nomeDaBanda, descricaoDaBanda, imgDaBanda)
-
-    //criar cards
-    let criaCard = document.createElement('div')
-    criaCard.innerHTML = `
-      <div class="card">
-        <img src= '${imgDaBanda}' class="card-img-top" alt="...">
-        <div class="card-body">
-          <h5 class="card-title">${nomeDaBanda}</h5>
-          <p class="card-text">${descricaoDaBanda}</p>
-          <a href="./view/detalhes.html" class="btn-abrir" target="_blank">Abrir</a>
-        </div>
-      </div>  
-    `
-    document.querySelector('.itens').appendChild(criaCard);
+  //adicionar envento ao clicar no botao 'adicionar'
+  FORMULARIO.addEventListener('submit', event => {
+      event.preventDefault()
+  
+      //criar cards
+      let criaCard = document.createElement('div')
+      criaCard.innerHTML = `
+        <div class="card">
+          <img src= '${IMAGEM_DA_BANDA.value}' class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">${NOME_DA_BANDA.value}</h5>
+            <p class="card-text">${DESCRICAO_DA_BANDA.value}</p>
+              <a href="./view/detalhes.html" class="btn-abrir" target="_blank">Abrir</a>
+          </div>
+        </div>  
+      `
+      document.querySelector('.itens').appendChild(criaCard)
   })
 }
-
